f5cffea6d2509042031c435e8ce43c84ef92ac8d
Ivar <Ivarottenheym@gmail.com>
2024-12-20 16:27:06 +0100

    added health check

---
+++ SOC-backend/SOC-backend.api/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SOC_backend.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
+
+
